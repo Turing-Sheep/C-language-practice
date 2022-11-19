6f2f6c8024735e504dbac67a548ae63161dc7c66
Turing-Sheep <dennyyang02062004@163.com>
2022-11-19 22:37:28 +0800

    C

---
+++ 11.19.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+const int N=100010,M=1000010;
+int n,m;
+int ne[N];
+char s[M],p[N];
+
+int main()
+{
+    cin>>n>>p+1>>m>>s+1;
+    for(int i=2,j=0;i<=n;i++)
+    {
+        while(j&&p[i]!=p[i+1])j=ne[j];
+        if(p[i]==p[j+1])j++;
+        ne[i]=j;
+    }
+    return 0;
+}
